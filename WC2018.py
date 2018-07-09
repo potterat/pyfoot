d29f2767adf9705b2f9429503ed88ff748df7196
Cédric Potterat <cedric.potterat@gmail.com>
2018-07-09 23:23:16 +0200

    new peoba.py

--- WC2018.py
+++ WC2018.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python
 import operator
-TOM = {'Q':['POL','ESP','FRA','AUT','ALL','ANG','BEL','ITA'],
-       'S':['BEL','ITA','FRA','ESP'],'F':['ESP','FRA'],'p':44,'t':0}
-CED = {'Q':['POL','ESP','FRA','AUT','ALL','ANG','BEL','ITA'],
-       'S':['ANG','ALL','FRA','ESP'],'F':['ALL','FRA'],'p':44,'t':0}
-JOA = {'Q':['POL','ESP','FRA','AUT','ALL','ANG','BEL','POR'],
-       'S':['ALL','ANG','FRA','ESP'],'F':['ESP','FRA'],'p':42,'t':0}
-LAN = {'Q':['POL','CRO','ITA','ALL','ESP','FRA','POR','ANG'],
-       'S':['CRO','ANG','ESP','FRA'],'F':['ESP','ANG'],'p':44,'t':0}
-ROD = {'Q':['POL','ITA','ALL','ESP','FRA','ANG','AUT','BEL'],
-       'S':['ALL','ANG','BEL','FRA'],'F':['FRA','ANG'],'p':41,'t':0}
-H=[['SUI','POL'],['CRO','POR'],['PDG','IRN'],['HON','BEL'],['ALL','SLO'],['ITA','ESP'],['FRA','IRL'],['ANG','ISL']]
+GIG = {'Q':['URU','BEL','FRA','SPA','SWI','DEN','GER','COL'],
+       'S':['BEL','DEN','FRA','GER'],'F':['GER','FRA'],'p':64,'t':0}
+KAR = {'Q':['URU','PER','BRA','SEN','SPA','FRA','GER','BEL'],
+       'S':['PER','BRA','FRA','GER'],'F':['GER','FRA'],'p':22,'t':0}
+JOA = {'Q':['POR','BRA','FRA','ENG','ARG','GER','BEL','SPA'],
+       'S':['ARG','ENG','FRA','GER'],'F':['ARG','FRA'],'p':58,'t':0}
+JUL = {'Q':['URU','CRO','BRA','GER','SPA','FRA','COL','BEL'],
+       'S':['URU','BRA','SPA','GER'],'F':['GER','BRA'],'p':57,'t':0}
+#ROD = {'Q':['POL','ITA','ALL','ESP','FRA','ANG','AUT','BEL'],
+#       'S':['ALL','ANG','BEL','FRA'],'F':['FRA','ANG'],'p':1,'t':0}
+H=[['URU','POR'],['FRA','ARG'],['BRA','MEX'],['BEL','JAP'],
+   ['SPA','RUS'],['CRO','DEN'],['SWE','SWI'],['COL','ENG']]
 
-TOMp = {}
+GIGp = {}
 JOAp = {}
-LANp = {}
+JULp = {}
 RODp = {}
-CEDp = {}
+KARp = {}
 
 SC = []
 DC = []
 FC = []
 i = 0
-PTS = {'TOM':0,'CED':0,'JOA':0,'LAN':0, 'ROD':0}
+PTS = {'GIG':0,'KAR':0,'JOA':0,'JUL':0}
 prob = {}
 tot = 0
 while i <256:
@@ -57,42 +58,35 @@ while i <256:
                 if win in ddF:
                     check += 1
             if check != len(FC): continue
-            PTS['TOM'] = TOM['p']+len(set(ddS).intersection(TOM['Q']))
-            PTS['CED'] = CED['p']+len(set(ddS).intersection(CED['Q']))
-            PTS['JOA'] = JOA['p']+len(set(ddS).intersection(JOA['Q']))
-            PTS['LAN'] = LAN['p']+len(set(ddS).intersection(LAN['Q']))
-            PTS['TOM'] += len(set(ddD).intersection(TOM['S']))
-            PTS['CED'] += len(set(ddD).intersection(CED['S']))
-            PTS['JOA'] += len(set(ddD).intersection(JOA['S']))
-            PTS['LAN'] += len(set(ddD).intersection(LAN['S']))
-            PTS['TOM'] += len(set(ddF).intersection(TOM['F']))
-            PTS['CED'] += len(set(ddF).intersection(CED['F']))
-            PTS['JOA'] += len(set(ddF).intersection(JOA['F']))
-            PTS['LAN'] += len(set(ddF).intersection(LAN['F']))
-            PTS['ROD'] = ROD['p']+len(set(ddS).intersection(ROD['Q']))
-            PTS['ROD'] += len(set(ddD).intersection(ROD['S']))
-            PTS['ROD'] += len(set(ddF).intersection(ROD['F']))
+            PTS['GIG'] = GIG['p']+2*len(set(ddS).intersection(GIG['Q']))
+            PTS['KAR'] = KAR['p']+2*len(set(ddS).intersection(KAR['Q']))
+            PTS['JOA'] = JOA['p']+2*len(set(ddS).intersection(JOA['Q']))
+            PTS['JUL'] = JUL['p']+2*len(set(ddS).intersection(JUL['Q']))
+            PTS['GIG'] += 3*len(set(ddD).intersection(GIG['S']))
+            PTS['KAR'] += 3*len(set(ddD).intersection(KAR['S']))
+            PTS['JOA'] += 3*len(set(ddD).intersection(JOA['S']))
+            PTS['JUL'] += 3*len(set(ddD).intersection(JUL['S']))
+            PTS['GIG'] += 4*len(set(ddF).intersection(GIG['F']))
+            PTS['KAR'] += 4*len(set(ddF).intersection(KAR['F']))
+            PTS['JOA'] += 4*len(set(ddF).intersection(JOA['F']))
+            PTS['JUL'] += 4*len(set(ddF).intersection(JUL['F']))
             sorted_PTS = sorted(PTS.items(),key=operator.itemgetter(1))
-            if PTS['LAN'] in LANp:
-                LANp[PTS['LAN']] += 1
+            if PTS['JUL'] in JULp:
+                JULp[PTS['JUL']] += 1
             else:
-                LANp[PTS['LAN']] = 1
-            if PTS['TOM'] in TOMp:
-                TOMp[PTS['TOM']] += 1
+                JULp[PTS['JUL']] = 1
+            if PTS['GIG'] in GIGp:
+                GIGp[PTS['GIG']] += 1
             else:
-                TOMp[PTS['TOM']] = 1
+                GIGp[PTS['GIG']] = 1
             if PTS['JOA'] in JOAp:
                 JOAp[PTS['JOA']] += 1
             else:
                 JOAp[PTS['JOA']] = 1
-            if PTS['ROD'] in RODp:
-                RODp[PTS['ROD']] += 1
+            if PTS['KAR'] in KARp:
+                KARp[PTS['KAR']] += 1
             else:
-                RODp[PTS['ROD']] = 1
-            if PTS['CED'] in CEDp:
-                CEDp[PTS['CED']] += 1
-            else:
-                CEDp[PTS['CED']] = 1
+                KARp[PTS['KAR']] = 1
             if sorted_PTS[len(sorted_PTS)-1][1]>sorted_PTS[len(sorted_PTS)-2][1]:
                 key =  sorted_PTS[len(sorted_PTS)-1][0]
                 if key in prob:
@@ -146,32 +140,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # normal distribution center at x=0 and y=5
-xn = ["TOM","LAN","CED","JOA","ROD"]
-x = [0,1,2,3,4,5]
-x1 = [0.5,1.5,2.5,3.5,4.5,5]
+xn = ["GIG","JUL","KAR","JOA"]
+x = [0,1,2,3,4]
+x1 = [0.5,1.5,2.5,3.5,4.5]
 from pylab import *
-y = [41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57]
+y = [41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,
+     58,59,60,61,62,63,64,65,66,67,68,69,70]
 zz = []
 for key in y:
     z = []
-    if key in TOMp:
-        z = z+[TOMp[key]/fullproba]
+    if key in GIGp:
+        z = z+[GIGp[key]/fullproba]
     else:
         z = z+ [0]
-    if key in LANp:
-        z = z+[LANp[key]/fullproba]
+    if key in JULp:
+        z = z+[JULp[key]/fullproba]
     else:
         z = z+ [0]
-    if key in CEDp:
-        z = z+[CEDp[key]/fullproba]
+    if key in KARp:
+        z = z+[KARp[key]/fullproba]
     else:
         z = z+ [0]
     if key in JOAp:
         z = z+[JOAp[key]/fullproba]
-    else:
-        z = z+ [0]
-    if key in RODp:
-        z = z+[RODp[key]/fullproba]
     else:
         z = z+ [0]
     zz = zz + [z]
